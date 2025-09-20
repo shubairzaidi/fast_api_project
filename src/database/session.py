907bf9dd50bbb3f2f2e499d22334fd3b91ae3669
unknown <mohd.shubair@sharajman.com>
2025-09-20 15:05:01 +0530

    updated

--- src/database/session.py
+++ src/database/session.py
@@ -1,9 +1,14 @@
-# src/database/session.py
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from src.database.models import Base
+import os
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://neondb_owner:npg_pj6nZqvVfdG4@ep-odd-math-adtyawv1-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
+load_dotenv()  # load variables from .env
+
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+if SQLALCHEMY_DATABASE_URL is None:
+    raise ValueError("DATABASE_URL environment variable not set")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
